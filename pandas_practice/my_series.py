fda0b39dccf82571e9ff4c0cbabf44b61007c0a7
shuhei0916 <shuheiito.stc@gmail.com>
2024-04-05 06:00:10 +0900

    modified my_series.py

--- pandas_practice/my_series.py
+++ pandas_practice/my_series.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
 
-a = pd.Series([60, 80, 70, 50, 30], index=["Japansese", "English", "Math", "Science", "History"])
+a = pd.Series([60, 80, 70, 50, 40], index=["Japansese", "English", "Math", "Science", "History"])
 print(type(a))
 print(a)
